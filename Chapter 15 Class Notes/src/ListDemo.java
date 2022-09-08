5c0cd55a6eb21c2da95669b13d4cc339d962f17b
MaxNguyen5040 <maxnguyen3012@gmail.com>
2022-09-08 11:49:08 -0500

    Started notes on linkedLists and listIterators

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -1,6 +1,8 @@
 import java.util.LinkedList;
 import java.util.ListIterator;
 
+import javax.print.event.PrintEvent;
+
 /**
  * This program demonstrates the LinkedList class
  * and ListIterator class.
@@ -9,5 +11,37 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
+        /*
+         * The addLast method can be used to populate a list.
+         * 
+         */
+        LinkedList<String> staff = new LinkedList<>();
+        staff.addLast("Diana");
+        staff.addLast("Harry");
+        staff.addLast("Romeo");
+        staff.addLast("Tom");
+
+        // This list is current: Diana, Harry, Romeo, Tom
+        /*
+         * The listIterator method creates a new list iterator that is positioned at the front of the list.
+         * 
+         * The "|" in the comment represents the iterator position
+         * 
+         */
+        ListIterator<String> iterator = staff.listIterator(); // |DHRT
+
+        /*
+         * the next method advanced the iterator over the next element in the list.
+         * 
+         */
+
+         iterator.next(); //D|HRT
+
+        /*
+         * The next method also returns the element that the iterator is passing
+         */
+        String name = iterator.next();
+        System.out.println("Expected: Harry");
+
     }
 }
