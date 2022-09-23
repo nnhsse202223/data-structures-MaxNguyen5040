2644c9c64e5d8e841c124cec61de168065904fea
MaxNguyen5040 <maxnguyen3012@gmail.com>
2022-09-23 10:56:08 -0500

    Finished Driveway

--- Chapter 15 Class Notes/src/PriorityQueueDemo.java
+++ Chapter 15 Class Notes/src/PriorityQueueDemo.java
@@ -10,6 +10,19 @@ public class PriorityQueueDemo
 {
     public static void main(String[] args)
     {
-        
+        // create a priority que of to do items
+        Queue<WorkOrder> q = new PriorityQueue<>();
+
+        q.add(new WorkOrder(3, "Vaccum"));
+        q.add(new WorkOrder(2, "Water plants"));
+        q.add(new WorkOrder(2, "make dinner"));
+        q.add(new WorkOrder(2, "walk dog"));
+        q.add(new WorkOrder(9, "play games"));
+        q.add(new WorkOrder(1, "take practice quiz"));
+
+        while(q.size() > 0){
+            System.out.println(q.remove());
+        }
+
     }
 }
